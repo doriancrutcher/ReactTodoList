513859812845aaaa99adca3c9534a202f26cae0a
Dorian Crutcher <doriancrutcher@Dorians-MBP.fios-router.home>
2020-08-10 21:21:11 -0400

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,92 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import Input from './Container/Input/Input';
+import ItemArea from './Components/ItemArea/ItemArea';
+import Header from './Container/Header/Header'
 
-function App() {
+class App extends Component {
+
+  constructor(props){
+    super(props)
+    this.state={
+        TaskList:[]
+    }
+    
+}
+IdCounter=0;
+
+RemoveItem=(Itemkey)=>{
+  console.log('remove me')
+  let KeyList=this.state.TaskList.map(x=>x.key)
+  let TaskListHolder=this.state.TaskList
+  TaskListHolder.splice(KeyList.indexOf(Itemkey),1)
+  console.log(TaskListHolder)
+ this.setState({TaskList:[...TaskListHolder]})
+      
+}
+
+AddItem=(Item)=>{
+  console.log()
+    this.IdCounter=this.IdCounter+=1;
+    let TaskListHolder=this.state.TaskList
+     console.log([...TaskListHolder])
+    this.setState({
+        TaskList:[...TaskListHolder,{
+            key:this.IdCounter,
+            Description:Item
+        }]
+    })
+
+
+}
+
+OldTasks=()=>{
+  this.setState(
+    {
+      TaskList:[...JSON.parse(localStorage.getItem('todoList'))]
+    }
+  )
+}
+
+
+componentWillMount(){
+
+  
+  if(localStorage.getItem('todoList')){
+    let ItemsStored=localStorage.getItem('todoList');
+    let ItemsParsed=JSON.parse(ItemsStored);
+    this.OldTasks()
+  }
+}
+componentDidUpdate(){
+  localStorage.setItem('todoList',JSON.stringify(this.state.TaskList))
+
+}
+
+
+render(){
   return (
+    <React.Fragment>
+<div className="movingBack">
+ 
+</div>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header/>
+
+      <Input AddItem={this.AddItem}></Input>
+      <div className="TodoArea">
+        {
+      (this.state.TaskList===[])?<p>Add Item</p>:(
+      this.state.TaskList.map((x)=>{
+        console.log(x.key)
+  return<ItemArea RemoveItem={this.RemoveItem} Key={x.key}
+    Item={x.Description}/>}))}
+      </div>
     </div>
+    </React.Fragment>
   );
+}
+
 }
 
 export default App;
